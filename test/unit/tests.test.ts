89015c0a643f04d59d883603912802dc700b35e9
Adam Desivi <desivi@desivi.com>
2020-06-06 02:05:37 +0200

    feat: [foo-002] add subtract

--- test/unit/tests.test.ts
+++ test/unit/tests.test.ts
@@ -1,9 +1,12 @@
 import { expect } from 'chai';
-
-import { add } from '../../src/index';
+import { add, subtract } from '../../src/index';
 
 describe('tests', () => {
   it('add', () => {
     expect(add(1, 2)).to.be.equal(3);
+  });
+
+  it('subtract', () => {
+    expect(subtract(2, 1)).to.be.equal(1);
   });
 });
